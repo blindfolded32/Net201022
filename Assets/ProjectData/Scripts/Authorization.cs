7711c78b3e65886018b442489d11e504c812f0d0
blindfolded32 <82173886+blindfolded32@users.noreply.github.com>
2022-11-07 19:07:22 +0300

    HW3

--- Assets/ProjectData/Scripts/Authorization.cs
+++ Assets/ProjectData/Scripts/Authorization.cs
@@ -2,22 +2,47 @@ using Photon.Pun;
 using Photon.Realtime;
 using PlayFab;
 using PlayFab.ClientModels;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Authorization : MonoBehaviourPunCallbacks
 {
     [SerializeField] private string _playFabTitle;
+    [SerializeField] private ConnectionPanel _connectionPanel;
+
+    private string _customID;
 
     void Start()
     {
-        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        _connectionPanel.SetOfflineConnectionStatus();
+        _connectionPanel.ConnectButton.interactable = false;
+
+        _connectionPanel.LoginButton.onClick.AddListener(Login);
+        _connectionPanel.ConnectButton.onClick.AddListener(Connect);
+        _connectionPanel.DisconnectButton.onClick.AddListener(Disconnect);
+    }
+
+    private void Login()
+    {
+        if (string.IsNullOrEmpty(_connectionPanel.GetLogin()))
+        {
+            _connectionPanel.LoginWarning();
+            return;
+        }
+        else
+        {
+            if(_connectionPanel.IsWarning)
+            {
+                _connectionPanel.ResetWarning();
+            }
+            _customID = _connectionPanel.GetLogin();
+        }
+
+        if(string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
             PlayFabSettings.staticSettings.TitleId = _playFabTitle;
 
         var request = new LoginWithCustomIDRequest
         {
-            CustomId = "TestUser",
+            CustomId = _customID,
             CreateAccount = true
         };
 
@@ -27,9 +52,14 @@ public class Authorization : MonoBehaviourPunCallbacks
                 Debug.Log(result.PlayFabId);
                 PhotonNetwork.AuthValues = new AuthenticationValues(result.PlayFabId);
                 PhotonNetwork.NickName = result.PlayFabId;
-                Connect();
+                                
+                _connectionPanel.ConnectButton.interactable = true;
+                _connectionPanel.DisconnectButton.interactable = true;
             },
-            error => Debug.LogError(error));
+            error =>
+            {
+                Debug.LogError(error);
+            });
     }
 
     private void Connect()
@@ -44,26 +74,47 @@ public class Authorization : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = PhotonNetwork.AppVersion;
-        }
+        }        
+    }
+
+    private void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        Debug.Log("OnConnectedToMaster");
+        Debug.Log("Connected To Master");
         if (!PhotonNetwork.InRoom)
             PhotonNetwork.JoinRandomOrCreateRoom(roomName: $"Room N{Random.Range(0, 9999)}");
+
+        _connectionPanel.SetOnlineConnectionStatus();
     }
 
     public override void OnCreatedRoom()
     {
         base.OnCreatedRoom();
-        Debug.Log("OnCreatedRoom");
+        Debug.Log("Created Room");
     }
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        Debug.Log($"OnJoinedRoom {PhotonNetwork.CurrentRoom.Name}");
+        Debug.Log($"Joined {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        Debug.Log("Disconnect");
+        _connectionPanel.SetOfflineConnectionStatus();
+    }
+
+    private void OnDestroy()
+    {
+        _connectionPanel.LoginButton.onClick.RemoveAllListeners();
+        _connectionPanel.ConnectButton.onClick.RemoveAllListeners();
+        _connectionPanel.DisconnectButton.onClick.RemoveAllListeners();
     }
 }
